bab54d5b86c0f3d31098defc26ebe8ac1dcd119f
bondfreak <perkbonde@hotmail.com>
2020-08-26 15:10:54 +0200

    Removed some code

--- script.py
+++ script.py
@@ -1,13 +1,5 @@
-import math
-import os
-import sys
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-s = "string"
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
